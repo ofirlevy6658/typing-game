284db747c8d1d0d99452121e65ee6993e8a471e3
ofirlevy6658 <ofirlevy6658@gmail.com>
2022-01-29 19:21:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  title = 'Typing Challenge';
+  successMsg = '';
+  randomText = faker.lorem.sentences(3);
+  enteredText = '';
+  onInput(value: string) {
+    this.enteredText = value;
+    if (value === this.randomText) this.successMsg = 'Success!';
+    else this.successMsg = '';
+  }
+  compare(letter: string, enteredLetter: string) {
+    if (!enteredLetter) return '';
+    return letter === enteredLetter ? 'correct' : 'wrong';
+  }
 }
